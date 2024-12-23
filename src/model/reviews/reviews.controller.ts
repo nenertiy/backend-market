1ac642f89773f218b3505f1ad3b2a9c7ece5999c
nenertiy <nenertiy@gmail.com>
2024-12-23 09:13:31 +0300

    add missing roles

--- src/model/reviews/reviews.controller.ts
+++ src/model/reviews/reviews.controller.ts
@@ -1,12 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ApiOperation } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('client')
   @Post()
   async createReview(@Body() dto: CreateReviewDto) {
     return this.reviewsService.createReview(dto);
@@ -30,6 +43,8 @@ export class ReviewsController {
     return this.reviewsService.getAllProductReviews(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('client')
   @Delete('id')
   async deleteReview(@Param('id') id: string) {
     return this.reviewsService.deleteReview(id);
